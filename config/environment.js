66585a9f8eef331f4e2556600ccbd63b81e1d28d
Perry Eising <perry.r.eising@gmail.com>
2015-09-27 21:20:46 -0700

    add files from weekend homework

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
+    firebase: 'https://fiery-torch-7755.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
